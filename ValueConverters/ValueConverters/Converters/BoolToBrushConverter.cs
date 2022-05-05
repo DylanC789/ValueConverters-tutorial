5aac4f0b16929a7617646d1b3a102b8513a4474d
DylanC789 <98245668+DylanC789@users.noreply.github.com>
2022-05-05 12:02:27 -0400

    Add files via upload

---
+++ ValueConverters/ValueConverters/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ValueConverters.Converters
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isOnline = (bool)value;
+            if (isOnline)
+                return Brushes.Green;
+            else
+                return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
